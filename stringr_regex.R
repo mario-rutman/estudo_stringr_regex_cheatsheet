0adb095fbaff63b0a5d843925756c402633f5674
mario-rutman <marrut@gmail.com>
2021-05-31 16:58:53 -0300

    Aumentando e corrigindo exemplos.

--- stringr_regex.R
+++ stringr_regex.R
@@ -1,13 +1,14 @@
 library(stringr)
 library(glue)
 library(tidyverse)
+library(htmlwidgets)
 
 
 # String: sequência de caracters entre aspas (simples ou duplas).
 # Tanto faz usar aspas duplas ou únicas.
 
 
-'a character string using single quotes'
+"a character string using single quotes"
 "a character string using double quotes"
 
 
@@ -16,9 +17,9 @@ char_vector <- character(0)
 
 
 # Agora preenchendo o char_vector.
-char_vector[2] <- 'coisa'
-char_vector[5] <- 'tio'
-char_vector[3] <- 'pincel'
+char_vector[2] <- "coisa"
+char_vector[5] <- "tio"
+char_vector[3] <- "pincel"
 print(char_vector)
 
 
@@ -27,22 +28,23 @@ length(char_vector)
 
 
 # Colar.
-paste('I', 'Love', 'R', sep = '-')
-paste("The Life of", pi, sep = ' ')
-paste('Casa', 1:5, sep = '.')
-paste('Casa', c("jaca", "amor", "lem"), sep = '***')
-paste('Casa', c("jaca", "amor", "lem"))
-paste('Casa', "jaca", "amor", "lem")
-paste(1:3, c("!", '&','*'), sep = '', collapse = '')
-paste(1:3, c("!", '&','*'), sep = '')
-paste(1:3, c("!", '&','*'), sep = '  ')
-paste(1:3, "!", '&','*')
+paste("I", "Love", "R", sep = "-")
+paste("The Life of", pi, sep = " ")
+paste("Casa", 1:5, sep = ".")
+paste("Casa", c("jaca", "amor", "lem"), sep = "***")
+paste("Casa", c("jaca", "amor", "lem"))
+paste("Casa", "jaca", "amor", "lem")
+paste(1:3, c("!", "&", "*"), sep = "", collapse = "")
+paste(1:3, c("!", "&", "*"), sep = "")
+paste(1:3, c("!", "&", "*"), sep = "  ")
+paste(1:3, "!", "&", "*")
 
 # Colar colapsando: pasteo.
 paste0("let's", "collapse", "all", "these", "words.")
-paste0('Vamos', 'colapsar', 'todas', 'estas', 'palavras.')
+paste0("Vamos", "colapsar", "todas", "estas", "palavras.")
 
-# Tem uma pacote novo, o glue, que faz o mesmo papel do paste, porém baseado no equivalento do Python,
+# Tem uma pacote novo, o glue, que faz o mesmo papel do paste, porém baseado no
+# equivalento do Python,
 # que é bastante simplificado.
 # O objetivo aqui é usar e criar exemplos do uso do glue.
 # O glue é uma versão para R da forma como se concatemam strings no Python.
@@ -53,42 +55,48 @@ library(glue)
 nome <- "Anita"
 idade <- 14
 aniversario <- as.Date("2021-02-11")
-glue('Meu nome é {nome},', 
-     ' minha idade ano que vem será {idade + 1} anos,', 
-     ' meu aniversário cai numa {format(aniversario, "%A, %B %d, %Y")}.')
+glue(
+  "Meu nome é {nome},",
+  " minha idade ano que vem será {idade + 1} anos,",
+  ' meu aniversário cai numa {format(aniversario, "%A, %B %d, %Y")}.'
+)
 
-glue('Meu nome é {nome},', 
-     ' minha idade ano que vem será {idade + 1}. Em {format(aniversario, "%Y")} 
-      meu aniversário cairá numa {format(aniversario, "%A")}.')
+glue(
+  "Meu nome é {nome},",
+  ' minha idade ano que vem será {idade + 1}. Em {format(aniversario, "%Y")}
+      meu aniversário cairá numa {format(aniversario, "%A")}.'
+)
 
 # Tal qual no Python basta ter uma aspa no início e outra no fim.
-glue('Meu nome é {nome}, minha idade ano que vem será {idade + 1} anos. Em {format(aniversario, "%Y")} 
+glue('Meu nome é {nome}, minha idade ano que vem será {idade + 1} anos. Em {format(aniversario, "%Y")}
       meu aniversário cairá numa {format(aniversario, "%A")}.')
 
 # As variáveis podem ficar dentro da função glue.
-glue('Meu nome é {nome}, minha idade ano que vem será {idade + 1}. Em {format(aniversario, "%Y")} 
-      meu aniversário cairá numa {format(aniversario, "%A")}.', 
-     nome = "Alice",
-     idade = 12, 
-     aniversario = as.Date("2021-09-29"))
+glue('Meu nome é {nome}, minha idade ano que vem será {idade + 1}. Em {format(aniversario, "%Y")}
+      meu aniversário cairá numa {format(aniversario, "%A")}.',
+  nome = "Alice",
+  idade = 12,
+  aniversario = as.Date("2021-09-29")
+)
 
 # Named arguments can be used to assign temporary variables.
-glue('My name is {name},',
-     ' my age next year is {age + 1},',
-     ' my anniversary is {format(anniversary, "%A, %B %d, %Y")}.',
-     name = "Joe",
-     age = 40,
-     anniversary = as.Date("2001-10-12"))
+glue("My name is {name},",
+  " my age next year is {age + 1},",
+  ' my anniversary is {format(anniversary, "%A, %B %d, %Y")}.',
+  name = "Joe",
+  age = 40,
+  anniversary = as.Date("2001-10-12")
+)
 
 # Podemos ainda usar a glue na criação de uma função.
-intro <- function(name, profession, country){
+intro <- function(name, profession, country) {
   glue("Meu nome é {name}, sou {profession} {country}")
 }
 
 intro("Maria", "defensora pública", "no Brasil")
 intro("Mário", "auditor fiscal", "no Brasil")
-intro('Alice', 'veterinária', 'no Brasil')
-intro('Anita', 'jornalista', 'em Portugal')
+intro("Alice", "veterinária", "no Brasil")
+intro("Anita", "jornalista", "em Portugal")
 
 
 # A glue aceita o pipe ( %>%).
@@ -143,11 +151,11 @@ toupper(c("All ChaRacterS in Upper Case", "abcde"))
 # Trocando os caracteres de um character.
 # chartr(o antigo, o novo, x)
 chartr("a", "A", "This is a boring string")
-chartr('l', 'b','Ela é louca pela lua')
+chartr("l", "b", "Ela é louca pela lua")
 
 # Múltiplas trocas.
 crazy <- c("Here's to the crazy ones", "The misfits", "The rebels")
-chartr("aei", "#!?", 'crazy') # troca o a por #, o e por ! e o i por ?
+chartr("aei", "#!?", "crazy") # troca o a por #, o e por ! e o i por ?
 
 # Abreviação: serve para os nomes ficarm menores.
 some_colors <- colors()[1:4]
@@ -193,17 +201,18 @@ elem2 <- "crazy"
 
 elem2 %in% set10
 elem1 %in% set10
-c('a', 'b', 'c') %in% c('a', 'j', 'b')
-c('a', 'j', 'b') %in% c('a', 'b', 'c')
+c("a", "b", "c") %in% c("a", "j", "b")
+c("a", "j", "b") %in% c("a", "b", "c")
 
 # Stringr package.
 library(stringr)
-library(htmlwidgets) #Para usar o str_view()
+library(htmlwidgets) # Para usar o str_view()
 
 # Começam sempre com 'str_'
-# Colar. 
+# Colar.
 str_c("May", "The", "Force", "Be", "With", "You")
-str_c(letters, collapse = "") 
+str_c(letters, collapse = "")
+str_c(letters, collapse = ", ")
 str_c("May", "The", "Force", NULL, "Be", "With", "You", character(0))
 str_dup(fruit, times = 2) # Duplica os strings individualmente.
 str_split_fixed(fruit, " ", n = 2) # Separa em duas colunas os nomes compostos. Quando tem um nome só nada acontece.
@@ -216,8 +225,8 @@ str_length(some_text)
 
 
 # Convertendo um vetor para factor.
-# Ontem ('2020-07-06') faleceu o Ennio Morricone. Segue minha R-omenagem.
-some_factor <- factor(c(1,1,1,2,1,2,1,3,1,1,2,3), labels = c("O bom,", "o mau", "e o feio."))
+# Ontem ('2020-07-06') faleceu o Ennio Morricone. Segue minha R-homenagem.
+some_factor <- factor(c(1, 1, 1, 2, 1, 2, 1, 3, 1, 1, 2, 3), labels = c("O bom,", "o mau", "e o feio."))
 some_factor
 
 # Verificando os tamanhos de some_factor.
@@ -225,11 +234,33 @@ str_length(some_factor)
 
 # String: sequência de caracters entre aspas (simples ou duplas).
 
+# Substituir NAs.
+str_replace_na(c("My Name", NA, "Jhon"),".")
+
+# Usaremos o objeto statemente em vários exemplos.
+statement<-c("R", "is powerful", "tool", "for data", "analysis")
+
+# Collapse.
+str_c(statement,collapse=" ")
+str_c(statement,collapse="@#$%")
+
+# O collapse tembém pode incluir palavras.
+str_c("test",1:10, sep="-")
+str_c("test",1:10, sep=" o que você quiser.")
+
+# str_sub seleciona uma parte uma string.
+str_sub(statement,1,5) #fica com a 1ª até a 5ª posição.
+str_sub(statement,2,4) # fica com a 2ª até a 4ª posição.
+
+# O str_subset seleciona um subconjunto de outro conjunto comforme um padrão.
+# No caso todas as cores que contêm a palavra 'green'.
+str_subset(colors(), pattern="green")
+
 # fruit vetor com 80 elementos no pacote stringr.
 glimpse(fruit)
 
-# 
-str_detect(fruit, 'a')
+#
+str_detect(fruit, "a")
 str_which(fruit, "a")
 str_count(fruit, "a")
 str_locate(fruit, "a")
@@ -247,28 +278,28 @@ str_subset(fruit, "  berry")
 str_extract(fruit, "[aeiou]")
 
 
-names <- c("Keisha", "Mohammed", "Jane", 'Anita', 'Alice', 'Mammmmeixa', 'Wilson', 'Márcia')
-str_sort(names) #Coloca em ordem alfabética.
-str_view(names, "^M") #identify strings that start with "M"
-str_view(names, 'a$') #identifica strings que terminam com 'a' minúsculo.
+names <- c("Keisha", "Mohammed", "Jane", "Anita", "Alice", "Mammmmeixa", "Wilson", "Márcia")
+str_sort(names) # Coloca em ordem alfabética.
+str_view(names, "^M") # identify strings that start with "M"
+str_view(names, "a$") # identifica strings que terminam com 'a' minúsculo.
 str_count(names, "^M") # conta em cada palavra os nomes que começam com M
 str_count(names, "m") # conta em cada palavra quantos 'm' tem.
-sum(str_count(names, 'a$')) # soma os nomes que terminam com 'a'.
+sum(str_count(names, "a$")) # soma os nomes que terminam com 'a'.
 
-colors <- c('red','orange','yellow','green','Blue','violet','#C8C8C8','#000000')
-str_detect(colors, '\\d')
-str_subset(colors, '^[a-z]') # faz um subconjunto dos elementos que começam com letra minúscula.
+colors <- c("red", "orange", "yellow", "green", "Blue", "violet", "#C8C8C8", "#000000")
+str_detect(colors, "\\d") # Detecta dígitos.
+str_subset(colors, "^[a-z]") # faz um subconjunto dos elementos que começam com letra minúscula.
 str_view_all(colors, "\\d+[A-Z]+")
-str_count(colors, '[a-zA-Z]') # conta o número de letras maiúsculas ou minúsculas. 
-str_view_all(colors, '^#') # string que começa com #.
-str_view_all(colors, '[a-z]') # mostra as letras minúsculas das .
+str_count(colors, "[a-zA-Z]") # conta o número de letras maiúsculas ou minúsculas.
+str_view_all(colors, "^#") # string que começa com #.
+str_view_all(colors, "[a-z]") # mostra as letras minúsculas das .
 
 
 # Mostra onde estão as vogais.
-str_view_all('Hoje é dia de esfiha!', "[aeiou]")
+str_view_all("Hoje é dia de esfiha!", "[aeiou]")
 
 # identify anything that's NOT a lowercase vowel
-str_view_all('Aqui mostra tudo que não é aeiou em minúsculas.', "[^aeiou]")
+str_view_all("Aqui mostra tudo que não é aeiou em minúsculas.", "[^aeiou]")
 
 endereços <- c("1234 Main Street", "1600 Pennsylvania Ave S/A", "Brick Buildinnnng")
 # identify anything that's a digit
@@ -307,13 +338,13 @@ str_view_all(endereços, "n{1}")
 ## identify any time n shows up exactly two times in a row
 str_view_all(endereços, "n{2}")
 
-## identify any time 'nn' shows up one or more times 
+## identify any time 'nn' shows up one or more times
 str_view_all(endereços, "nn+")
 
-## identify any time n shows up two or three times 
+## identify any time n shows up two or three times
 str_view_all(endereços, "n{2,3}")
 
-## identify any time n shows up three or four times 
+## identify any time n shows up three or four times
 str_view_all(endereços, "n{3,4}")
 
 sentences <- "Montreal, é prá lá que vou com Maria, Anita e Alice."
@@ -323,10 +354,10 @@ str_to_title(sentences)
 
 
 str_replace(fruit, "a", "X-X") # Só troca o primeiro que aparece.
-str_replace(c('terra', 'casa', 'texto', 'carro'), 't', '$') 
-str_replace(c('terra', 'casa', 'texto', 'carro'), 'ca', '*****') 
+str_replace(c("terra", "casa", "texto", "carro"), "t", "$")
+str_replace(c("terra", "casa", "texto", "carro"), "ca", "*****")
 str_replace_all(fruit, "a", "T") # Troca todos.
-str_replace_all(c('terra', 'casa', 'texto', 'carro', 'banana'), 'a', 'OSCAR')
+str_replace_all(c("terra", "casa", "texto", "carro", "banana"), "a", "OSCAR")
 
 # Este é bom para fazer muitas trocas.
 frutas <- c("one apple", "two pears", "three bananas")
@@ -346,7 +377,7 @@ str_extract_all(shopping_list, "\\d")
 
 
 # Para usar o str_view é necessário o pacote htmlwidgets.
-library(htmlwidgets)
+
 
 # Vou usar o str_view_all para ver os significados dos regex.
 # A  parte marcada seria  o resultado do str_extract_all()
@@ -359,35 +390,35 @@ str_view_all("abc ABC 123\t.!?\\(){}\n", "\\(")
 str_view_all("abc ABC 123\t.!?\\(){}\n", "\\)")
 str_view_all("abc ABC 123\t.!?\\(){}\n", "\\\\")
 str_view_all("abc ABC 123\t.!?\\(){}\n", "\\{")
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\}") #Em suma:o símbolo depois de \\ é marcado/extraído.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\n") #Nova linha.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\t") #t de tab, que é maior que simples espaço.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\s") #s de space, espaço.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\d") #d de dígito, número inteiro.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\w") #w letra  e  número.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\}") # Em suma:o símbolo depois de \\ é marcado/extraído.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\n") # Nova linha.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\t") # t de tab, que é maior que simples espaço.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\s") # s de space, espaço.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\d") # d de dígito, número inteiro.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\w") # w letra  e  número.
 
 
-#Quando usamos o formato [:alguma palavra:] nos inserimos numa classificação.
+# Quando usamos o formato [:alguma palavra:] nos inserimos numa classificação.
 # Existem espaço e escrita: [:space:] ou [:graph:].
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:space:]") #É nova linha, espaço ou tab.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:blank:]") #É só espaço ou tab.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:graph:]") #É tudo que não é space, i.é. qualquer escrita.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:space:]") # É nova linha, espaço ou tab.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:blank:]") # É só espaço ou tab.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:graph:]") # É tudo que não é space, i.é. qualquer escrita.
 
-#[:graph:] pode ser [:punct:] ou [:alnum:].
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:punct:]") #Pontuação só espaço ou tab.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:alnum:]") #Alfabético ou numérico.
+# [:graph:] pode ser [:punct:] ou [:alnum:].
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:punct:]") # Pontuação só espaço ou tab.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:alnum:]") # Alfabético ou numérico.
 
-#[:alnum:] pode ser [:digit:] ou [:alpha:].
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:digit:]") #Números.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:alpha:]") #Letras.
+# [:alnum:] pode ser [:digit:] ou [:alpha:].
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:digit:]") # Números.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:alpha:]") # Letras.
 
-#[:alpha:] pode ser [:lower:] ou [:upper:].
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:lower:]") #Pontuação só espaço ou tab.
-str_view_all("abc ABC 123\t.!?\\(){}\n", "[:upper:]") #Alfabético ou numérico.
+# [:alpha:] pode ser [:lower:] ou [:upper:].
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:lower:]") # Pontuação só espaço ou tab.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "[:upper:]") # Alfabético ou numérico.
 
-#Por fim o ponto ., que engloba tudo menos nova linha. 
-str_view_all("abc ABC 123\t.!?\\(){}\n", "\\n") #Nova linha.
-str_view_all("abc ABC 123\t.!?\\(){}\n", ".") #Tudo que não é nova linha.
+# Por fim o ponto ., que engloba tudo menos nova linha.
+str_view_all("abc ABC 123\t.!?\\(){}\n", "\\n") # Nova linha.
+str_view_all("abc ABC 123\t.!?\\(){}\n", ".") # Tudo que não é nova linha.
 
 # ou
 str_view_all("abcde", "ab|d")
@@ -403,8 +434,9 @@ str_view_all("abcde", "[^ce]")
 str_view_all("abcde", "[^b]")
 
 # range ou intervalo.
-str_view_all("abcde", "[a-d]")
-str_view_all("abcde", "[^a-d]")
+str_view_all("abcde", "[a-d]") # Seleciona abcd.
+str_view_all(abcde, "[abcd]")
+str_view_all("abcde", "[^a-d]") # Seleciona tudo que não é abcd.
 
 # Âncoras: Começa com ^, termina com $.
 str_view_all("aaa", "^a")
@@ -412,10 +444,10 @@ str_view_all("aaa", "a$")
 
 # Look arounds.
 # Seguido de...
-str_view_all("bacad", "a(?c)")
+str_view_all("bacad", "a(?=c)") # a seguido de c.
 
 # Não seguido de...
-str_view_all("bacad", "a(?!c)")
+str_view_all("bacad", "a(?!c)") # a não seguido de c.
 
 # Precedido de...
 str_view_all("bacad", "(?<=b)a")
@@ -444,3 +476,117 @@ str_view_all(".a.aa.aaa", "a{2,}")
 # Entre n e m vezes.
 str_view_all(".a.aa.aaa.aaaa.aaaaa", "a{2,4}")
 str_view_all(".a.aa.aaa.aaaa.aaaaa", "a{1,3}")
+
+
+# Vamos agora trabalhar um texto dado no curso de Jurimetria.
+texto <- "Avenida Angélica, 754, apto 55, bairro Santa_Cecília, São Paulo, cep 01227-000, telefone (11)55555-6874"
+
+## str_extract
+
+# Duas contra barras \\ chama-se escape.
+# Usa-se quando queremos que prevaleça a literalidade do sinal.
+# Por exemplo, se quero um ponto de interrogação em seu sentido literal, isto é,
+# indicando uma pergunta, escrevo '\\?'.
+# Se vou usar a interrogação significando '0 ou 1 vez' (regex) basta '?'.
+
+# A procura pelo padrão começa no início do texto
+# Uma vez encontrado o padrão a busca termina.
+str_extract(texto, "\\d")
+
+# Se quero que a busca continue tenho que indicar '+'.
+str_extract(texto, "\\d+")
+
+# Se quero que se estenda a todos str)extract_all.
+str_extract_all(texto, "\\d+")
+
+
+# Busca 5 dígitos, seguido de '-', seguido de 3 dígitos e seguido de vírgula.
+str_extract_all(texto, "\\d{5}-\\d{3}(?=,)")
+
+# Abre parenteses, 2 dígitos, fecha parenteses, 4 ou 5 dígitos, hífen
+# e 4 dígitos.
+# Depois que nos acostumamos parece até um pouco natural.
+str_extract(texto, "\\(\\d{2}\\)\\d{4,5}-\\d{4}")
+str_view_all(texto, "\\(\\d{2}\\)\\d{4,5}-\\d{4}")
+
+# 1 ou mais dígitos.
+str_extract("04/07/2020", "\\d+")
+str_extract_all("04/07/2020", "\\d+")
+
+# Precedido de barra, 1 ou mais dígitos.
+str_extract("04/07/2020", "(?<=/)\\d+")
+str_extract_all("04/07/2020", "(?<=/)\\d+")
+
+# Começando a busca pelo fim '$', um ou mais dígitos.
+str_extract("04/07/2020", "\\d+$")
+
+# w minúsculo: todos alnum. Ou,
+# graph (tudo que usa 'tinta') fora punct.
+str_extract_all(texto, "\\w+")
+str_view_all(texto, "\\w+")
+
+# W maiúsculo: punct e space.
+str_extract_all(texto, "\\W+")
+str_view_all(texto, "\\W+")
+
+# Procuro por o que está precedido por apto, espaço, algo com 1 a 5 dígitos,
+# vírgula e espaço.
+str_extract(texto, "(?<=apto\\s\\d{1,5},\\s).+?(?=,)")
+str_view_all(texto, "(?<=apto\\s\\d{1,5},\\s).+?(?=,)")
+
+ceps <- c("012176-987", "87632000")
+
+str_extract(ceps, "\\d{5}-?\\d{3}")
+
+# Tudo com 1 ou mais dígitos.
+str_extract_all(texto, "\\d+")
+str_view_all(texto, "\\d+")
+
+# Tudo com 1 ou mais dígitos, não seguido de hífen.
+str_extract_all(texto, "\\d+(?!-)")
+str_view_all(texto, "\\d+(?!-)")
+
+# Começando do fim, o primeiro número com 1 ou mais dígitos.
+str_extract(texto, "\\d+$")
+str_view(texto, "\\d+$")
+
+# str_replace.
+# Significa no texto, ao encontrar 1 ou mais dígitos,
+# precedido de apto e espaço, trocar pelo '57'.
+# No str_replace é importante notar que o 57, o elemento que substituí,
+# NÃO É PARA SER ESCRITO COMO REGEX!!!
+
+
+
+str_replace_all(texto, "(?<=apto\\s)\\d+", "57")
+str_replace_all(texto, "(?<=apto\\s)\\d+", "Surfando na Angélica?")
+
+# Neste exemplo o ponto foi tomado em sua literalidade \\.
+str_replace("jurimetria.tjsp", "\\.", "[[]]")
+
+# Já neste o ponto teve o sentido de qualquer coisa.
+# Isto é, a primeira qualquer coisa que encontrou substituiu.
+str_replace("jurimetria.tjsp", ".", "%&*")
+
+
+# Aqui todos os conjuntos de 1 ou mais dígitos será sujbstituído por xxxx.
+str_replace_all(texto, "\\d+", "xxxx")
+
+# Substituir por nada ou remover dá no mesmo.
+str_replace_all(texto, "\\d+", "")
+str_remove_all(texto, "\\d+")
+
+### str_detect
+str_detect(texto, "bairro")
+str_detect(texto, "cidade")
+
+str_detect(ceps, "-", negate = FALSE) # Negar falsamente é confirmar.
+str_detect(ceps, "-", negate = TRUE) # Negar verdadeiramente é efetivamente negar.
+
+## str_subset
+str_subset(ceps, "-", negate = TRUE)
+
+
+## str_which
+
+str_which(ceps, "-")
